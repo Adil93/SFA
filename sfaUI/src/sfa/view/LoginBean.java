7f4fcef1a983d3bdab9bb59c2c642f8e43e13230
Sreeraj Thottipparambil <sreeraj.thottipparambil@oracle.com>
2016-11-11 11:23:31 +0530

    Login Authentication and autherisation added

--- sfaUI/src/sfa/view/LoginBean.java
+++ sfaUI/src/sfa/view/LoginBean.java
@@ -1,5 +1,20 @@
 package sfa.view;
 
+import javax.faces.context.FacesContext;
+import javax.faces.context.ExternalContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.security.auth.callback.CallbackHandler;
+import javax.servlet.RequestDispatcher;
+import javax.security.auth.Subject;
+
+import javax.servlet.http.HttpSession;
+
+import weblogic.security.SimpleCallbackHandler;
+import weblogic.security.services.Authentication;
+
+import weblogic.servlet.security.ServletAuthentication;
+
 public class LoginBean {
     
     private String username;
@@ -22,6 +37,28 @@ public class LoginBean {
     }
     
     public String doLogin() {
+        
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
+        CallbackHandler handler = new SimpleCallbackHandler(this.username, this.password.getBytes());
+        Subject subject;
+        
+        try {
+            subject = Authentication.login(handler);
+            ServletAuthentication.runAs(subject, request);
+            ServletAuthentication.generateNewSessionID(request);
+            String successUrl = "/adfAuthentication?success_url=/faces/Home.jspx";
+            HttpServletResponse response = (HttpServletResponse) externalContext.getResponse();
+            RequestDispatcher dispatcher = request.getRequestDispatcher(successUrl);
+            HttpSession session = request.getSession();
+            dispatcher.forward(request, response);
+            facesContext.responseComplete();
+            
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        
         return  null; 
     }
 }
